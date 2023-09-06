6e826ad3a388d3800d33b3ea70c33135258c493b
Christian Shaffer <calexandershaffer@gmail.com>
2023-09-05 21:34:09 -0400

    Add rainbow selector

--- scripts.js
+++ scripts.js
@@ -1,11 +1,57 @@
 const grid = document.querySelector('.grid');
-const clearButton = document.querySelector('.button.clear');
+const rainbowButton = document.querySelector('.button.rainbow');
 const eraserButton = document.querySelector('.button.eraser');
+const clearButton = document.querySelector('.button.clear');
 const slider = document.querySelector("#range");
 const sliderValue = document.querySelector('.value');
 
 let tableVectorSize = 16; // default value
 let sideLength = 40; // default value
+
+const rainbowColors = [
+    "#FF0000", // Red
+    "#FF4500", // OrangeRed
+    "#FFA500", // Orange
+    "#FFD700", // Gold
+    "#FFFF00", // Yellow
+    "#ADFF2F", // GreenYellow
+    "#7FFF00", // Chartreuse
+    "#7CFC00", // LawnGreen
+    "#00FF00", // Lime
+    "#32CD32", // LimeGreen
+    "#228B22", // ForestGreen
+    "#008000", // Green
+    "#20B2AA", // LightSeaGreen
+    "#40E0D0", // Turquoise
+    "#00CED1", // DarkTurquoise
+    "#00FFFF", // Aqua
+    "#00BFFF", // DeepSkyBlue
+    "#1E90FF", // DodgerBlue
+    "#0000FF", // Blue
+    "#4169E1", // RoyalBlue
+    "#8A2BE2", // BlueViolet
+    "#9370DB", // MediumPurple
+    "#DA70D6", // Orchid
+    "#EE82EE", // Violet
+    "#FF00FF", // Fuchsia
+    "#FF69B4", // HotPink
+    "#FF1493", // DeepPink
+    "#FFB6C1", // LightPink
+    "#FFC0CB", // Pink
+    "#FFDAB9", // PeachPuff
+    "#FFDEAD", // NavajoWhite
+    "#FFE4E1", // MistyRose
+    "#FFE4B5", // Moccasin
+    "#FFF8DC", // Cornsilk
+    "#F0E68C", // Khaki
+    "#EEE8AA", // PaleGoldenrod
+    "#F5DEB3", // Wheat
+  ];
+
+function pickRandomRainbowColor () {
+    let selectedColor = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+    return selectedColor;
+}
 
 let selectedColor = '';
 const eraserColor = '#FFF8DC';
@@ -22,6 +68,10 @@ eraserButton.addEventListener("click", function() {
     setColor(eraserColor);
 });
 
+rainbowButton.addEventListener("click", function() {
+    setColor(pickRandomRainbowColor());
+})
+
 slider.addEventListener("input", (e) => {
     tableVectorSize = e.target.value;
     sliderValue.textContent = tableVectorSize + " x " + tableVectorSize;
@@ -37,6 +87,9 @@ function draw () {
         square.style.height = sideLength + "px";
         square.style.width = sideLength + "px";
         grid.appendChild(square.cloneNode(true));
+        if (rainbowColors.includes(selectedColor)) {
+            square.style.backgroundColor = rainbowColors[0];
+        }
     }
     setColor(selectedColor);
 }
@@ -53,6 +106,11 @@ function setColor (color) {
             square.onmouseover = () => square.style.backgroundColor = eraserColor;
         });
         selectedColor = eraserColor;
+    } else if (rainbowColors.includes(color)) {
+        squares.forEach(square => {
+            square.onmouseover = () => square.style.backgroundColor = pickRandomRainbowColor();
+        });
+        selectedColor = color;
     }
 }
 
